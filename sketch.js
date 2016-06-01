bac2f3286c2751c9afe9e71570034df93858e867
Janak Dadhaniya <janakdadhaniya@Janaks-MacBook-Pro.local>
2016-05-31 20:15:51 -0700

    flashing random patterns

--- sketch.js
+++ sketch.js
@@ -1,35 +1,57 @@
-
-var noiseScale = 0.02;
+var noiseScale = 0.01;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(198,127,255)
+  noStroke()
   
+  
+
+
+      
+  
+
 }
 
 function draw(){
-  noStroke()
-  background(198,127,255)
-  for (var x = 0; x <=width; x+=200){
-  for (var y = 0; y<=height; y+=110){
-    drawSquare1(x,y)
-    drawSquare2(x,y)
-  }
   
+  var binary = [0,1];
+  for (var x = 0; x <=width; x+=110){
+    for (var y = 0; y<=height; y+=110){
+    var index = floor(random(binary.length));
+    chooseOne(binary,index,x,y)
+      
+    }
+    
   }
 }
+  
+  
 
 
+function chooseOne(binary, index,x,y){
+
+      
+        if (binary[index] == 0){
+          drawSquare2(x,y)
+        }
+        else if (binary[index] == 1){
+          drawSquare1(x,y)
+        }
+    
+}
 
 function drawSquare1(x,y) {
   var noiseVal = noise((mouseX+x)*noiseScale, (mouseY+y)*noiseScale)
   fill(214,121,noiseVal*100)
-  rect(x+100,y,100,100);
-  
+ // rect(x+110,y,100,100);
+    rect(x,y,100,100);
+
   fill(198,127,noiseVal*255)
-  rect(x+110,y+10,80,80);
+  rect(x+10,y+10,80,80);
   
   fill(224,121,noiseVal*100)
-  rect(x+140,y+40,20,20) 
+  rect(x+40,y+40,20,20) 
   }
   
 function drawSquare2(x,y) {
