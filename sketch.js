854952cd5186094fc8fafd253fe2be08ec411a90
Janak Dadhaniya <janakdadhaniya@Janaks-MacBook-Pro.local>
2016-05-31 22:08:36 -0700

    randomizer working

--- sketch.js
+++ sketch.js
@@ -4,26 +4,22 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(198,127,255)
   noStroke()
-  
-  
+    var binary = [0,1];
 
-
+    
+    for (var x = 0; x <=width; x+=105){
+      for (var y = 0; y<=height; y+=105){
+        var index = floor(random(binary.length));
+        chooseOne(binary,index,x,y)
+    
+      }
       
-  
-
+    }
 }
 
 function draw(){
   
-  var binary = [0,1];
-  for (var x = 0; x <=width; x+=110){
-    for (var y = 0; y<=height; y+=110){
-    var index = floor(random(binary.length));
-    chooseOne(binary,index,x,y)
-      
-    }
-    
-  }
+
 }
   
   
@@ -31,14 +27,12 @@ function draw(){
 
 function chooseOne(binary, index,x,y){
 
-      
         if (binary[index] == 0){
           drawSquare2(x,y)
         }
         else if (binary[index] == 1){
           drawSquare1(x,y)
         }
-    
 }
 
 function drawSquare1(x,y) {
@@ -64,8 +58,6 @@ function drawSquare2(x,y) {
   //small square
   fill(224,121,noiseVal*20)
   rect(x+10,y+10,80,80);
-  
-  
   
   // /
   fill(204,57,noiseVal*255)
