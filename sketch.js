23b2c112adce255ac34383a1ce09ea1592ec89db
Janakd <janakd@gmail.com>
2016-06-02 11:15:07 -0700

    comments and starting to implement delay

--- sketch.js
+++ sketch.js
@@ -8,47 +8,49 @@ function setup() {
 }
 
 function draw(){
-    var binary = [0,1,2,3,4,5,6,8,8,8,8,8,8,8,8,8,8,8,8,8];
+    var binary = [0,1];
 
-    
     for (var x = 0; x <=width; x+=105){
       for (var y = 0; y<=height; y+=105){
         
         var index = floor(random(binary.length));
         chooseOne(binary,index,x,y)
-    
+        //implement delay
       }
       
     }
 
 }
-  
-  
 
-
+//Chooses one of two squares to draw
 function chooseOne(binary, index,x,y){
 
-        if (binary[index] == 0){
+        if (index == 0){
           drawSquare2(x,y)
         }
-        else if (binary[index] ==1){
+        else if (index ==1){
           drawSquare1(x,y)
         }
 }
 
+//Draws square [.]
 function drawSquare1(x,y) {
-  var noiseVal = noise((mouseX+x)*noiseScale, (mouseY+y)*noiseScale)
+    var noiseVal = noise((mouseX+x)*noiseScale, (mouseY+y)*noiseScale)
+
+  //big square
   fill(214,121,noiseVal*100)
- // rect(x+110,y,100,100);
     rect(x,y,100,100);
 
+  //med square
   fill(198,127,noiseVal*255)
   rect(x+10,y+10,80,80);
   
+  //small square
   fill(224,121,noiseVal*100)
   rect(x+40,y+40,20,20) 
   }
-  
+
+//Draws square [X]
 function drawSquare2(x,y) {
   var noiseVal = noise((mouseX+x)*noiseScale, (mouseY+y)*noiseScale)
   
